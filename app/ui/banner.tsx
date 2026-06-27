e24913c07b1b7816179913c6df0d74a78005b9e1
Hamish Hamilton-Smith <hamishhamiltonsmith@gmail.com>
2026-06-27 23:53:53 +0100

    Cleanup

--- app/ui/banner.tsx
+++ app/ui/banner.tsx
@@ -4,9 +4,9 @@ export function Banner() {
     return (
         <div className="absolute top-0 w-full py-16 md:py-20 lg:py-25">
             <Image
-                className="dark:invert"
-                src="/next.svg"
-                alt="Next.js logo"
+                className="dark:invert mx-auto"
+                src="/logo.png"
+                alt="STARI LOGO"
                 width={100}
                 height={20}
                 priority
