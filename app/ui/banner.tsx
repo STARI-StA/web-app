935e40d880af1763cc8647aa21f4adddc22d96f6
Hamish Hamilton-Smith <hamishhamiltonsmith@gmail.com>
2026-06-28 00:29:19 +0100

    .

--- app/ui/banner.tsx
+++ app/ui/banner.tsx
@@ -2,15 +2,20 @@ import Image from "next/image";
 
 export function Banner() {
     return (
-        <div className="absolute top-0 w-full py-16 md:py-20 lg:py-25">
-            <Image
-                className="dark:invert mx-auto"
-                src="/logo.png"
-                alt="STARI LOGO"
-                width={100}
-                height={20}
-                priority
-            />
+        <div className="absolute top-0 w-full py-10 md:py-14 lg:py-19 bg-background">
+            <span className="absolte mx-auto flex flex-col text-center items-center gap-3">
+                <Image
+                    className="dark:invert"
+                    src="/logo.png"
+                    alt="STARI LOGO"
+                    width={200}
+                    height={40}
+                    priority
+                />
+                <span className="text-subtle">
+                    St Andrews Aerospace
+                </span>
+            </span>
         </div>
     );
 }
